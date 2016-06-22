70df0c23045736612df12d99c9edaeb67b482998
Mark Tiderman <mark@streamlinedstudio.com>
2016-06-22 18:55:04 -0400

    added v2

---
+++ v2/README.md
@@ -0,0 +1,19 @@
+StreamlinedStudio
+=================
+
+
+Bootstrap
+http://twitter.github.com/bootstrap/index.html
+
+Isotope
+http://isotope.metafizzy.co/
+
+CSS3 Microsoft Modern Buttons
+http://ace-subido.github.com/css3-microsoft-metro-buttons/index.html
+
+Metro style Forms
+
+Google map:
+http://gmap.nurtext.de/
+
+Anchor
---
+++ v2/js/streamlined-custom.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function () {
+    //$('#dynamolove').child().width('');
+
+
+
+
+
+    /*---- MAP ----*/
+    $("#map").gMap({
+        controls: false,
+        scrollwheel: false,
+        draggable: true,
+        markers: [{
+            latitude: 44.47698,
+            longitude: 1006.8,
+            icon: {
+                image: "img/pin.png",
+                iconsize: [32, 47],
+                iconanchor: [32, 47],
+                infowindowanchor: [12, 0]
+            }
+        }
+        ],
+        icon: {
+            image: "img/pin.png",
+            iconsize: [26, 46],
+            iconanchor: [12, 46],
+            infowindowanchor: [12, 0]
+        },
+        latitude: 39.130,
+        longitude: -84.512,
+        zoom: 10,
+    });
+
+
+
+    /*Carousel----Start*/
+
+    $('#myCarousel').carousel({
+        interval: 5000
+    });
+
+    $('#carousel-text').html($('#slide-content-0').html());
+
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        var id = id_selector.substr(id_selector.length - 1);
+        var id = parseInt(id);
+        $('#myCarousel').carousel(id);
+    });
+
+
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid', function (e) {
+        var id = $('.item.active').data('slide-number');
+        $('#carousel-text').html($('#slide-content-' + id).html());
+    });
+
+    /*Carousel----End*/
+
+
+
+});
+
+
